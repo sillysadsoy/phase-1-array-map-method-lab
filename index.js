fb2e1f50daa66215026f5604af6619899c3cd7da
roger chat <mattrogerpelosi@gmail.com>
2021-08-12 13:26:27 -0400

    commit tree 411d534b1b9d4a123c8bffc3be7de820dd279213

--- index.js
+++ index.js
@@ -12,5 +12,20 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((string) => {
+    const indWords = string.split(' ');
+    const combine = indWords.map(word => word.charAt(0).toUpperCase() + word.slice(1, word.length));
+    const upperCased = combine.join(' ');
+    return upperCased;
+  });
 }
+
+// const titleCased = () => {
+//   const title = tutorials.map(function (name) {
+//     const words = name.split(' ');
+//     const combine = words.map(word => word.charAt(0).toUpperCase() + word.slice(1, word.length));
+//     const cased = combine.join(' ');
+//     return cased;
+//   });
+//   return title;
+// }
